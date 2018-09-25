0253f8dd29f48a31626c1ffc3926e669a10d6ac5
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2018-09-25 12:51:13 -0400

    added CMakeLists.txt

--- header.h
+++ header.h
@@ -10,7 +10,11 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 #include <SDL2/SDL_opengl.h>
+#ifdef __APPLE__
+#include <OpenGL/glu.h>
+#else
 #include <GL/glu.h>
+#endif
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <string>
